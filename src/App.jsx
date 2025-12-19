099de7f070a18005c75a2d012ad407c4dbd1ffdf
Davi Amaral <dasoitaperuna@gmail.com>
2025-12-19 18:31:32 -0300

    Tela de ERRO 404 criada

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Items from './pages/Items'
 import Home from './pages/Home'
 import ItemDetails from './pages/ItemDetails'
+import NotFound from './pages/NotFound'
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/items" element={<Items />} />
           <Route path="/items/:id" element={<ItemDetails />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>

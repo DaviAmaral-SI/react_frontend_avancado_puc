3058a367a22708db46399541430dd342c9603421
Davi Amaral <dasoitaperuna@gmail.com>
2025-12-20 10:40:23 -0300

    Componente reutilizável de Alerta Modal criado

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState } from "react";
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
@@ -7,9 +8,11 @@ import SendIcon from '@mui/icons-material/Send';
 import XIcon from '@mui/icons-material/X';
 import Swal from 'sweetalert2';
 import Tooltip from "@mui/material/Tooltip";
+import ModalAlert from './ModalAlert';
 
 
 export default function SimpleBottomNavigation() {
+  const [showAlert, setShowAlert] = useState(false);
   const [value, setValue] = React.useState('recents');
 
   const handleChange = (event, newValue) => {
@@ -17,20 +20,23 @@ export default function SimpleBottomNavigation() {
   };
 
   return (
+    /* Componente de Barra inferior */
     <BottomNavigation sx={{ width: 500 }} value={value} onChange={handleChange}>
+
+      {/* Componente de Alerta com Timer */}
+      <ModalAlert
+        show={showAlert}
+        title="Compartilhado com sucesso!"
+        icon="success"
+        timer={2000}
+      />
 
       <Tooltip title="Enviar para e-mail" arrow>
         <BottomNavigationAction
           label="Share"
           value="Share"
           icon={<SendIcon />}
-          onClick={() => 
-            Swal.fire({
-              title: "Enviado para o seu e-mail!",
-              icon: "success",
-              timer: 2000
-            })
-          }
+          onClick={() => setShowAlert(true)}
           sx={{
               "&:hover": {
                 color: "#1976d2",
@@ -44,7 +50,7 @@ export default function SimpleBottomNavigation() {
           label="WhatsApp"
           value="WhatsApp"
           icon={<WhatsAppIcon />}
-          onClick={() => window.open("https://web.whatsapp.com", "_blank")}
+          onClick={() => setShowAlert(true)}
           sx={{
               "&:hover": {
                 color: "#1976d2",
@@ -58,7 +64,7 @@ export default function SimpleBottomNavigation() {
           label="Instagram"
           value="Instagram"
           icon={<InstagramIcon />}
-          onClick={() => window.open("https://www.instagram.com/", "_blank")}
+          onClick={() => setShowAlert(true)}
           sx={{
               "&:hover": {
                 color: "#1976d2",
@@ -72,7 +78,7 @@ export default function SimpleBottomNavigation() {
           label="X" 
           value="X" 
           icon={<XIcon />} 
-          onClick={() => window.open("https://x.com/", "_blank")}
+          onClick={() => setShowAlert(true)}
           sx={{
               "&:hover": {
                 color: "#1976d2",
